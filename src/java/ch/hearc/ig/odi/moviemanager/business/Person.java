563c33c43b7a5de4b9b0ff598034bb9d40099dd3
Boris Klett <boris.klett@he-arc.ch>
2016-05-06 15:02:09 +0200

    Importation de la class Services fait

--- src/java/ch/hearc/ig/odi/moviemanager/business/Person.java
+++ src/java/ch/hearc/ig/odi/moviemanager/business/Person.java
@@ -1,41 +1,79 @@
 package ch.hearc.ig.odi.moviemanager.business;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
  * Class business (personne);
- * 
- * @author boris.klett
+ *
+ * @author boris.klett <boris.klett@he-arc.ch>
  */
 public class Person {
 
-    private String identifiant;
+    private Long identifiant;
     private String firstName;
     private String lastName;
-    private List movies;
+    private Map<Long, Movie> movies = new HashMap<Long, Movie>();
 
+    /**
+     * Constructor of the Person class, not parameterized
+     */
     public Person() {
     }
 
-    public Person(String identifiant, String firstName, String lastName, List movies) {
+    public Person(Long identifiant, String firstName, String lastName, Map<Long, Movie> movies) {
         this.identifiant = identifiant;
         this.firstName = firstName;
         this.lastName = lastName;
         this.movies = movies;
     }
 
-    public Person(String identifiant, String firstName, String lastName) {
+    public Person(Long identifiant, String firstName, String lastName) {
         this.identifiant = identifiant;
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public String getIdentifiant() {
+    /**
+     * Used to add a movie in the watched movies list of a person
+     *
+     * @param movie The movie that we want to add in the watched movies list of a person
+     * @return The movie added
+     */
+    public Movie addMovie(Movie movie) {
+        if (this.movies.containsKey(movie.getId())) {
+            //Raise une exception unique
+            //throw new UniqueException(movie.getId());
+            return null;
+        }
+        this.movies.put(movie.getId(), movie);
+        return movie;
+    }
+
+    /**
+     * Used to remove a movie from the watched movies list of a person (ex.: if there is an error and 
+     * we want to remove a movie from the list of movies of a person because he/she actually has never seen it ).
+     * 
+     * @param movie The movie that we want to remove from the watched movies list of a person 
+     * @return The removed movie
+     */
+    public Movie removeMovie(Movie movie) {
+        if (!this.movies.containsKey(movie.getId())) {
+            //Raise une exception n'existe pas
+            //throw new NotExistException(movie.getId());
+            return null;
+        }
+        this.movies.remove(movie.getId());
+        return movie;
+    }
+
+    public Long getIdentifiant() {
         return identifiant;
     }
 
-    public void setIdentifiant(String identifiant) {
+    public void setIdentifiant(Long identifiant) {
         this.identifiant = identifiant;
     }
 
@@ -55,11 +93,11 @@ public class Person {
         this.lastName = lastName;
     }
 
-    public List getMovies() {
+    public Map<Long, Movie> getMovies() {
         return movies;
     }
 
-    public void setMovies(List movies) {
+    public void setMovies(Map<Long, Movie> movies) {
         this.movies = movies;
     }
 
