44a59b0a91b5ae0365f47aa5226cad0f34b306ab
Melissa Voirol <melissa.voirol@he-arc.ch>
2016-05-06 20:16:30 +0200

    Ajout liste personnes
    Ajout de la liste de personnes + DuplicateElementException

--- src/java/ch/hearc/ig/odi/moviemanager/business/Movie.java
+++ src/java/ch/hearc/ig/odi/moviemanager/business/Movie.java
@@ -1,19 +1,24 @@
 package ch.hearc.ig.odi.moviemanager.business;
 
+import ch.hearc.ig.odi.moviemanager.exception.DuplicateElementException;
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
  * @author Melissa Voirol <melissa.voirol@he-arc.ch>
  */
 public class Movie implements Serializable {
-    
+
     private Long id;
     private String name;
     private String producer;
-    
+    private List<Person> people;
+
     /**
      * Mandatory parameterized constructor of the Movie class
+     *
      * @param id Unique number of the movie
      * @param name Name of the movie
      * @param producer Producer of the movie
@@ -22,8 +27,31 @@ public class Movie implements Serializable {
         this.id = id;
         this.name = name;
         this.producer = producer;
+        this.people = new LinkedList<>();
     }
-    
+
+    /**
+     * Add a new person to the movie and returns an instance of that person
+     *
+     * @param id Unique number of the person
+     * @param fn Firstname of the person
+     * @param ln Lastname of the person
+     * @return The new person
+     * @throws DuplicateElementException
+     */
+    public Person addPerson(Long id, String fn, String ln) throws DuplicateElementException {
+
+        Person pers = null;
+
+        if (!people.contains(id)) {
+            pers = new Person(id, fn, ln);
+            people.add(pers);
+        } else {
+            throw new DuplicateElementException("A person with that id already exists");
+        }
+        return pers;
+    }
+
     public Long getId() {
         return id;
     }
@@ -47,5 +75,9 @@ public class Movie implements Serializable {
     public void setProducer(String producer) {
         this.producer = producer;
     }
-    
+
+    public List<Person> getPeople() {
+        return people;
+    }
+
 }
