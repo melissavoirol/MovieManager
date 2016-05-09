2fb8a0629fccf4318f23b095c99aec6284f28bd0
Boris Klett <boris.klett@he-arc.ch>
2016-05-09 18:03:31 +0200

    La navigation entre la home page et le détail d'une personne est fonctionnelle

--- src/java/ch/hearc/ig/odi/moviemanager/presentation/bean/PeopleDisplayBean.java
+++ src/java/ch/hearc/ig/odi/moviemanager/presentation/bean/PeopleDisplayBean.java
@@ -2,14 +2,11 @@ package ch.hearc.ig.odi.moviemanager.presentation.bean;
 
 import ch.hearc.ig.odi.moviemanager.business.Person;
 import ch.hearc.ig.odi.moviemanager.service.Services;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -27,19 +24,13 @@ public class PeopleDisplayBean implements Serializable {
     @Inject
     Services services;
     private static final Logger LOGGER = Logger.getLogger(PeopleDisplayBean.class.getName());
-    private final FileHandler fileLogHandler;
     private List<Person> people;
 
     /**
      * Create new instance of PeopleDisplayBean controller
      *
-     * @throws java.io.IOException
      */
-    public PeopleDisplayBean() throws IOException {
-        fileLogHandler = new FileHandler("movieManager_log.log");
-        fileLogHandler.setFormatter(new SimpleFormatter());
-        fileLogHandler.setLevel(Level.SEVERE);
-        LOGGER.addHandler(fileLogHandler);
+    public PeopleDisplayBean(){
     }
 
     /**
